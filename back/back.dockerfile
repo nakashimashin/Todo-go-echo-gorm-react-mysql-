cf756a43653b011b347088073d287018b331804e
nakashimashin <nakashimashin0517@gmail.com>
2023-12-10 17:49:34 +0900

    コンテナを起動すると上手く動作しない

--- back/back.dockerfile
+++ back/back.dockerfile
@@ -1,8 +1,11 @@
 FROM golang:1.21
 
+
 WORKDIR /back
 
 COPY go.mod go.sum ./
 RUN go mod download && go mod verify
 
 CMD ["go", "run", "main.go"]
+
+EXPOSE 8080
---
+++ back/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	if err := e.Start(":8080"); err != nil {
+		e.Logger.Fatal(err)
+	}
+}
