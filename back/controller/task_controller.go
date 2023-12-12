91f421db5cf0bf7bdf18c50687e8f1e8807afa19
nakashimashin <nakashimashin0517@gmail.com>
2023-12-13 02:02:03 +0900

    delete関数を修正

--- back/controller/task_controller.go
+++ back/controller/task_controller.go
@@ -3,10 +3,13 @@ package controller
 import (
 	"back/db"
 	"back/model"
+	"errors"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
 )
 
 func CreateTask(c echo.Context) error {
@@ -38,19 +41,38 @@ func UpdateTask(c echo.Context) error {
 	if err := c.Bind(&task); err != nil {
 		return err
 	}
-
 	if err := db.DB.Model(&task).Updates(model.Task{Title: task.Title, UpdatedAt: time.Now()}).Error; err != nil {
 		return err
 	}
-
 	return c.JSON(http.StatusOK, task)
 }
 
 func DeleteTask(c echo.Context) error {
-	task := model.Task{}
-	if err := c.Bind(&task); err != nil {
-		return err
+	// URLパラメータからIDを取得
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		// IDが無効な場合はエラーを返す
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid task ID")
 	}
-	db.DB.Delete(&task)
+
+	// 削除するタスクを取得
+	task := model.Task{}
+	result := db.DB.First(&task, id)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		// タスクが見つからない場合はエラーを返す
+		return echo.NewHTTPError(http.StatusNotFound, "Task not found")
+	} else if result.Error != nil {
+		// その他のデータベースエラーがある場合はエラーを返す
+		return echo.NewHTTPError(http.StatusInternalServerError, "Database error")
+	}
+
+	// タスクを削除
+	result = db.DB.Delete(&model.Task{}, id)
+	if result.Error != nil {
+		// 削除中にエラーが発生した場合は、エラーを返す
+		return echo.NewHTTPError(http.StatusInternalServerError, "Error deleting task")
+	}
+
+	// 削除されたタスクの内容をJSON形式で返す
 	return c.JSON(http.StatusOK, task)
 }
