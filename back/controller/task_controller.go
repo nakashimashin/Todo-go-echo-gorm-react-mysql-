ca1fa0e1ea33a4f9d623519873b8096e3771161f
nakashimashin <nakashimashin0517@gmail.com>
2023-12-13 02:31:31 +0900

    UpdateTaskを指定したidのタスクを更新するように変更

--- back/controller/task_controller.go
+++ back/controller/task_controller.go
@@ -51,14 +51,39 @@ func GetTask(c echo.Context) error {
 }
 
 func UpdateTask(c echo.Context) error {
-	task := model.Task{}
-	if err := c.Bind(&task); err != nil {
+	// URLパラメータからIDを取得
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		// IDが無効な場合はエラーを返す
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid task ID")
+	}
+
+	// タスクの新しい内容を取得
+	newTaskData := model.Task{}
+	if err := c.Bind(&newTaskData); err != nil {
 		return err
 	}
-	if err := db.DB.Model(&task).Updates(model.Task{Title: task.Title, UpdatedAt: time.Now()}).Error; err != nil {
+
+	// UpdatedAtを現在時刻に設定
+	newTaskData.UpdatedAt = time.Now()
+
+	// 指定されたIDのタスクを更新
+	result := db.DB.Model(&model.Task{}).Where("id = ?", id).Updates(newTaskData)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		// タスクが見つからない場合はエラーを返す
+		return echo.NewHTTPError(http.StatusNotFound, "Task not found")
+	} else if result.Error != nil {
+		// その他のデータベースエラーがある場合はエラーを返す
+		return echo.NewHTTPError(http.StatusInternalServerError, "Database error")
+	}
+
+	// 更新されたタスクを取得して返す
+	updatedTask := model.Task{}
+	if err := db.DB.First(&updatedTask, id).Error; err != nil {
 		return err
 	}
-	return c.JSON(http.StatusOK, task)
+
+	return c.JSON(http.StatusOK, updatedTask)
 }
 
 func DeleteTask(c echo.Context) error {
