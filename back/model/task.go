9d56d429fafdf980d0a6071767ea9e345db15374
nakashimashin <nakashimashin0517@gmail.com>
2023-12-13 12:11:54 +0900

    Task型の設定変更

--- back/model/task.go
+++ back/model/task.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type Task struct {
-	ID        uint      `json:"id" param:"id"`
-	Title     string    `json:"title"`
+	ID        uint      `json:"id" gorm:"primaryKey" param:"id"`
+	Title     string    `json:"title" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
