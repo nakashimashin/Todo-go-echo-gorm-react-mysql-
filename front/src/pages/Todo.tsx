ffd08c02a0a6715709072b17ac6166d8c05f9835
nakashimashin <nakashimashin0517@gmail.com>
2023-12-14 17:41:46 +0900

    Form,label,checkboxのコンポーネントを追加(front)

--- front/src/pages/Todo.tsx
+++ front/src/pages/Todo.tsx
@@ -1,10 +1,9 @@
-import { Button } from '@/components/ui/button'
+// import { Button } from '@/components/ui/button'
 
 export const Todo = () => {
   return (
     <div>
       <div>todo</div>
-      <Button>Click me</Button>
     </div>
   )
 }
