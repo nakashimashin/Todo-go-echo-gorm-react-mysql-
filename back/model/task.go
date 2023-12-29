0dedeb794d3aa4bf32c6118a95d6ce2e77a382a7
nakashimashin <nakashimashin0517@gmail.com>
2023-12-29 17:10:39 +0900

    Task構造体にUserIDを追加(back)

--- back/model/task.go
+++ back/model/task.go
@@ -5,6 +5,7 @@ import "time"
 type Task struct {
 	ID        uint      `json:"id" gorm:"primaryKey" param:"id"`
 	Title     string    `json:"title" gorm:"not null"`
+	UserID    uint      `json:"user_id" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
