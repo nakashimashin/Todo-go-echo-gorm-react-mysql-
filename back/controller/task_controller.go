1f395c9abb323329379c1367a56779949e71d84d
nakashimashin <nakashimashin0517@gmail.com>
2023-12-30 00:08:52 +0900

    UserIdに対応してCRUD(back)

--- back/controller/task_controller.go
+++ back/controller/task_controller.go
@@ -6,14 +6,23 @@ import (
 	"errors"
 	"net/http"
 	"strconv"
-	"time"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 )
 
+func getUserIDFromToken(c echo.Context) uint {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	userID := claims["user_id"].(uint)
+	return userID
+}
+
 func CreateTask(c echo.Context) error {
-	task := model.Task{}
+	userID := getUserIDFromToken(c)
+
+	task := model.Task{UserID: userID}
 	if err := c.Bind(&task); err != nil {
 		return err
 	}
@@ -22,18 +31,18 @@ func CreateTask(c echo.Context) error {
 }
 
 func GetTasks(c echo.Context) error {
+	userID := getUserIDFromToken(c)
+
 	tasks := []model.Task{}
-	db.DB.Find(&tasks)
+	db.DB.Where("user_id = ?", userID).Find(&tasks)
 	return c.JSON(http.StatusOK, tasks)
 }
 
 func GetTask(c echo.Context) error {
-	id, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, "Invalid task ID")
-	}
-	task := model.Task{}
-	result := db.DB.First(&task, id)
+	userID := getUserIDFromToken(c)
+	id, _ := strconv.Atoi(c.Param("id"))
+	var task model.Task
+	result := db.DB.Where("id = ? AND user_id = ?", id, userID).First(&task)
 	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
 		return echo.NewHTTPError(http.StatusNotFound, "Task not found")
 	} else if result.Error != nil {
@@ -43,44 +52,50 @@ func GetTask(c echo.Context) error {
 }
 
 func UpdateTask(c echo.Context) error {
+	userID := getUserIDFromToken(c)
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, "Invalid task ID")
 	}
-	newTaskData := model.Task{}
-	if err := c.Bind(&newTaskData); err != nil {
-		return err
-	}
-	newTaskData.UpdatedAt = time.Now()
-	result := db.DB.Model(&model.Task{}).Where("id = ?", id).Updates(newTaskData)
-	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-		return echo.NewHTTPError(http.StatusNotFound, "Task not found")
-	} else if result.Error != nil {
+
+	var existingTask model.Task
+	if err := db.DB.Where("id = ? AND user_id = ?", id, userID).First(&existingTask).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return echo.NewHTTPError(http.StatusNotFound, "Task not found")
+		}
 		return echo.NewHTTPError(http.StatusInternalServerError, "Database error")
 	}
 
-	updatedTask := model.Task{}
-	if err := db.DB.First(&updatedTask, id).Error; err != nil {
+	updateData := model.Task{}
+	if err := c.Bind(&updateData); err != nil {
 		return err
 	}
-	return c.JSON(http.StatusOK, updatedTask)
+	updateData.ID = existingTask.ID
+	updateData.UserID = existingTask.UserID
+
+	if err := db.DB.Model(&existingTask).Updates(updateData).Error; err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Error updating task")
+	}
+
+	return c.JSON(http.StatusOK, updateData)
 }
 
 func DeleteTask(c echo.Context) error {
+	userID := getUserIDFromToken(c)
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, "Invalid task ID")
 	}
-	task := model.Task{}
-	result := db.DB.First(&task, id)
-	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-		return echo.NewHTTPError(http.StatusNotFound, "Task not found")
-	} else if result.Error != nil {
+
+	var task model.Task
+	if err := db.DB.Where("id = ? AND user_id = ?", id, userID).First(&task).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return echo.NewHTTPError(http.StatusNotFound, "Task not found")
+		}
 		return echo.NewHTTPError(http.StatusInternalServerError, "Database error")
 	}
 
-	result = db.DB.Delete(&model.Task{}, id)
-	if result.Error != nil {
+	if err := db.DB.Delete(&model.Task{}, id).Error; err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "Error deleting task")
 	}
 
