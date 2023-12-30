41b768ed2f6d7f9dd9dffd4aa8767e6a95547d68
nakashimashin <nakashimashin0517@gmail.com>
2023-12-30 12:37:01 +0900

    backで環境変数を追加した(back)

--- back/db/db.go
+++ back/db/db.go
@@ -2,7 +2,9 @@ package db
 
 import (
 	"back/model"
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -12,15 +14,14 @@ var DB *gorm.DB
 var err error
 
 func init() {
-	// dbUser := os.Getenv("DB_USER")
-	// dbPassword := os.Getenv("DB_PASSWORD")
-	// dbHost := os.Getenv("DB_HOST")
-	// dbPort := os.Getenv("DB_PORT")
-	// dbName := os.Getenv("DB_NAME")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
 
-	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-	// 	dbUser, dbPassword, dbHost, dbPort, dbName)
-	dsn := "shin:password@tcp(db:3306)/mysql?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser, dbPassword, dbHost, dbPort, dbName)
 
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
